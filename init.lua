94f11532ab786912197fedf9839594a0eefff58d
GreenBlob <lightboxfacts@gmail.com>
2023-08-22 22:23:44 +0800

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,28 +1,30 @@
 local markdown_guide = [[
-# Minetest Capture the Flag Guide
+# Capture the Flag Guide
+
 ### Basics
-Click the opposite team's flag to take it. Click your team's flag before the enemy takes in order to to capture.
+* Click the opposite team's flag to take it. Click your team's flag before the enemy takes in order to to capture.
+* Each team has a team chest; put useful items in there and **never** take more than you need!
 
 ### Modes
-CTF cycles through three modes: Classes, Nade Fight, and Classic. 
+* CTF cycles through three modes: Classes, Nade Fight, and Classic. 
 
-1. In Classes, right click your team's flag to change classes. Reading each class' weapon description is helpful.
-2. In Nade Fight, right click your grenade to switch between three types of nade.
-3. In Classic, eat apples to heal and find ores to craft better swords.
+* In Classes, right click your team's flag to change classes. It's helpful to read the description of the items your class is given.
+* In Nade Fight, right click your grenade to switch between three types of nade.
+* In Classic, eat apples to heal and find ores to craft better swords.
 
 ### Tips & Tricks
-1. Start your messages with /t to talk to your team.
-2. Eat apples to quickly restore your health.
-3. Sprint by pressing the fast key (E).
-4. Use /r to check your rank.
-6. Use /m to add a team marker at pointed location.
-7. Use /s to check the current and previous match summary.
-8. Use /top50 to see the leaderboard.
-9. Use /team to check all team members.
-10. Press sneak while jumping to jump up two blocks.
-11. Use /donate <player> <score> to donate score to a player.
-12. Use /lb to see a list of bountied players.
-13. Use /msg <player> to send a PM to a player.
+* Press sneak while jumping to jump up two blocks.
+* Start your messages with /t to talk to your team.
+* Sprint by pressing the fast key (E).
+* Use /r to check your rank.
+* Use /m to add a team marker at pointed location.
+* Use /s to check the current and previous match summary.
+* Use /top50 to see the leaderboard.
+* Use /team to check all team members.
+* Use /donate <player> <score> to donate score to a player.
+* Use /lb to see a list of bountied players.
+* Use /msg <player> to send a PM to a player.
+
 ]]
 
 
